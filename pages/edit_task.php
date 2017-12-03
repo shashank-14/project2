91061f7efc686a855bd583e8adae7dfea40b8089
shashank shashikant parab <sp2339@afsconnect1.njit.edu>
2017-12-03 17:28:50 -0500

    Adding remaining files

---
+++ pages/edit_task.php
@@ -0,0 +1,4 @@
+
+<form action="index.php?page=tasks&action=delete&id=<?php $data['id'] ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+</form>
